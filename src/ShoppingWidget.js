92f9b54bf75a8730c7115c671cbc9c89909daafb
Ali Spittel <ali07cat07@gmail.com>
2018-04-11 09:07:58 -0400

    Add interactive button & font awesome

--- src/ShoppingWidget.js
+++ src/ShoppingWidget.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react'
 import './ShoppingWidget.css'
 
+const ShoppingButton = ({ inCart, buttonColor, toggleInCart }) => {
+  let content = inCart ? (
+    <span>
+      <i className='fas fa-cart' />
+      &nbsp;In Cart
+    </span>
+
+  ) : (
+    <span>
+      <i className='fas fa-cart-plus' />
+      &nbsp;Add To Basket
+    </span>
+  )
+
+  return (
+    <button
+      style={{ backgroundColor: buttonColor }}
+      onClick={toggleInCart}
+    >
+      {content}
+    </button>
+  )
+}
+
 class ShoppingWidget extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      inCart: false
+    }
+  }
+
+  toggleInCart () {
+    this.setState(prevState => ({
+      inCart: !prevState.inCart
+    }))
+  }
+
   render () {
     return (
       <div className='card'>
-        <img src={this.props.picture} />
+        <img src={this.props.picture} alt={this.props.title} />
         <div className='description'>
           <h3 className='item-name'>{this.props.title}</h3>
           <h4 className='item-price'>${this.props.price}.00</h4>
         </div>
-        <button style={{ backgroundColor: this.props.buttonColor }}>Add To Basket</button>
+        <ShoppingButton
+          inCart={this.state.inCart}
+          toggleInCart={this.toggleInCart.bind(this)}
+          buttonColor={this.props.buttonColor}
+        />
       </div>
     )
   }
